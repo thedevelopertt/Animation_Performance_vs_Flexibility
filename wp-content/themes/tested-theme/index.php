f7289eb5ed0bfbc915e5022eba0a1a382f604434
Greg <greigdillon@gmail.com>
2019-09-11 16:07:26 -0500

    Added logo navbar

--- wp-content/themes/tested-theme/index.php
+++ wp-content/themes/tested-theme/index.php
@@ -2,10 +2,22 @@
 <html>
 <head>
     <meta name="viewport" content="initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri())?>/dist/css/style.css">
+    <link rel="stylesheet" href="<?php use App\BlogInformation;
+
+    echo esc_url(get_template_directory_uri())?>/dist/css/style.css">
     <?php wp_head(); ?>
 </head>
 <body>
+
+<nav class="landing-nav">
+    <div class="container-fluid">
+        <div class="nav-row">
+            <div class="landing-logo">
+                <img src="<?php echo BlogInformation::getSvgPath(); ?>logo.svg" alt="">
+            </div>
+        </div>
+    </div>
+</nav>
 
 <section class="landing-section">
     <div class="container-fluid">
@@ -15,7 +27,7 @@
             </div>
 
             <div class="landing-lead">
-                <p>France-based interactive design & creative web studio</p>
+                <h1>France-based interactive design & creative web studio</h1>
             </div>
 
             <div class="landing-action">
