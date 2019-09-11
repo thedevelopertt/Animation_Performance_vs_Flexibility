bd89ff0ad8640e06d66af3f46807c080afafa218
Greg <greigdillon@gmail.com>
2019-09-11 16:18:19 -0500

    Added landing page svg

--- wp-content/themes/tested-theme/index.php
+++ wp-content/themes/tested-theme/index.php
@@ -21,9 +21,11 @@
 
 <section class="landing-section">
     <div class="container-fluid">
-        <div class="row">
-            <div>
-
+        <div class="landing-row">
+            <div class="landing-svg">
+                <svg viewBox="0 0 100 100">
+                    <path fill="#00C198" stroke-width="0" d="M12 50c0-21 17-38 38-38s38 17 38 38-17 38-38 38" style="stroke-dasharray: 179.113px; stroke-width: 24px; transform-origin: 50% 50%; stroke-dashoffset: 0px; transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1);"></path>
+                </svg>
             </div>
 
             <div class="landing-lead">
